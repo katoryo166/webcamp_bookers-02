b49fa2696ac82dad21e9d1266676ab77c39540db
EC2 Default User <ec2-user@ip-172-31-15-22.ec2.internal>
2021-07-20 10:16:21 +0000

    book.user.name

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -37,8 +37,12 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to books_path
+    if  @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to books_path
+    else
+      render "edit"
+    end
   end
 
   private
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f0245c787c327e39d9e1545fae975454385c4bf15c20a6abd518d266fb5a562a31cd7db83c32c8c4fd05fc282d15b1e28d35139f33c0f32f18297eaadea7be30'
